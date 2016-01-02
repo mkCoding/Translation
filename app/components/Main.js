51bfcdeecab048ba208d5abb5d2fbfe0bf1feb79
Mike K <mkem50@gmail.com>
2016-01-01 22:23:25 -0800

    added bootstrap framework, css file and modified Main.js

--- app/components/Main.js
+++ app/components/Main.js
@@ -6,8 +6,20 @@ var ReactDOM = require('react-dom');
 var Main = React.createClass({
     render: function(){
         return (
+
 <div>
-    Hello World
+<h2>Translation</h2>
+<div className="col-xs-4">
+  <label for="ex1">Enter text to translate</label>
+  <input className="form-control" id="ex1" placeholder="Text input" type="text"/>
+  <button className="btn btn-default" type="submit">Translate</button>
+</div>
+
+
+
+
+
+
 </div>
 );
     }
