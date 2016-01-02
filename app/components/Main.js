7154bda067d83d602fc94104685f0984c1c1350a
Mike K <mkem50@gmail.com>
2016-01-01 17:09:38 -0800

    first commit

---
+++ app/components/Main.js
@@ -0,0 +1,16 @@
+/*eslint-env node*/
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Main = React.createClass({
+    render: function(){
+        return (
+<div>
+    Hello World
+</div>
+);
+    }
+});
+
+ReactDOM.render(<Main />, document.getElementById('app'));
